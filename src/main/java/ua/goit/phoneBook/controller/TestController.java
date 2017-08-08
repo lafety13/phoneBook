b277dedc05001795d923cf56178953a352cd0b69
vadim <kozak13vadim13@bigmir.net>
2017-08-08 16:58:24 +0300

    Created the spring web project

---
+++ src/main/java/ua/goit/phoneBook/controller/TestController.java
@@ -0,0 +1,13 @@
+package ua.goit.phoneBook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+}
