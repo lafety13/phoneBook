3a3544afe5a3e56fc8e34ee2bf1573ecea4303ba
vadim <kozak13vadim13@bigmir.net>
2017-08-10 14:38:06 +0300

    changed main class. Now main class can work with file of setting, set sys properties and setting spring profile from properties

--- src/main/java/ua/goit/phoneBook/WebApplication.java
+++ src/main/java/ua/goit/phoneBook/WebApplication.java
@@ -2,17 +2,48 @@ package ua.goit.phoneBook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.core.env.AbstractEnvironment;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
 
 @SpringBootApplication
-public class WebApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(WebApplication.class);
+public class WebApplication {
+    public static void main(String[] args) {
+        setUp();
+        SpringApplication.run(WebApplication.class, args);
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(WebApplication.class, args);
+    private static void setUp() {
+        String path = System.getProperty("lardi.conf");
+
+        Properties properties = new Properties();
+        InputStream stream = null;
+        try {
+            stream = new FileInputStream(path);
+            properties.load(stream);
+        } catch (FileNotFoundException e) {
+            //logger
+        } catch (IOException e) {
+            //logger
+        }
+
+        //setting config properties as system variables
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            System.setProperty((String) entry.getKey(), (String) entry.getValue());
+        }
+
+        //setting spring profile from properties
+        String profile = properties.getProperty("profile");
+        if (profile != null) {
+            System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, profile);
+            if (profile.equals("mysql")) {
+                System.setProperty("GENERATED_KEY_NAME", "GENERATED_KEY");
+            }
+        }
     }
 }
--- src/main/java/ua/goit/phoneBook/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package ua.goit.phoneBook.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
