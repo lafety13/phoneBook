9a42a5de0b618cf83e72f39593bd0f7aa662bf92
vadim <kozak13vadim13@bigmir.net>
2017-08-12 01:20:01 +0300

    added a two method findAll and update to the generic service interface

--- src/main/java/ua/lardi/phoneBook/service/GenericService.java
+++ src/main/java/ua/lardi/phoneBook/service/GenericService.java
@@ -1,9 +1,15 @@
 package ua.lardi.phoneBook.service;
 
+import java.util.List;
+
 public interface GenericService<T> {
     T save(T object);
 
     T findById(long id);
 
+    void update(T object);
+
     void delete(long id);
+
+    List<T> findAll();
 }
